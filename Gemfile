86ea14198d50f8ba389fbc8e6e4306d5026af3d1
Andrew Page <andrewpage@Steve-Pages-MacBook-Pro.local>
2014-01-18 13:49:27 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+
+gem 'bootstrap-sass-rails' # Bootstrap
+gem 'font-awesome-rails'
+
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+gem 'devise'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :production do
+	gem 'unicorn-rails'
+	gem 'pg'
+end
+
+group :development, :test do
+	gem 'sqlite3'
+	gem 'better_errors'
+	gem 'rspec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smashlistings::Application.config.secret_key_base = '3826401b9eb1176a9f91aabf8909e9951d7aa7bb2b4cb5dc301d87e11b9609130099ec4b692e24e0e2876d7db1c01fc365ff94fbbe83f8497ab460b325c29bf4'
