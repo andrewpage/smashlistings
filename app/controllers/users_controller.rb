721052aa243cfb4f79c53372a207bb9197874643
Andrew Page <andrew@andrewpage.me>
2014-01-18 15:58:36 -0800

    User profie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:username, :first_name,
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,5 @@
 module UserHelper
+	def full_name(user)
+		return "%s %s" % [user.first_name, user.last_name]
+	end
 end
